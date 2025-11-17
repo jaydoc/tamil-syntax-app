308026be0a813614546be9c4d5713df062dcaca7
jaydoc <jegenk@gmail.com>
2025-11-17 01:21:15 -0600

    Build to docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tamil-syntax-app/', // important for GitHub Pages
+  // your repo name here
+  base: '/tamil-syntax-app/',
+  build: {
+    outDir: 'docs',
+  },
 })
